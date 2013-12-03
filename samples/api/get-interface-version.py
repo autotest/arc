5ee942e1a24e16d3fca5ef72050047dc3dba81af
Cleber Rosa <crosa@redhat.com>
2013-12-03 10:44:57 -0300

    samples/api/get-interface-version.py: use autotest.shared module
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- samples/api/get-interface-version.py
+++ samples/api/get-interface-version.py
@@ -1,13 +1,11 @@
 import sys
 
 import arc.connection
-import arc.defaults
 import arc.host
 
 c = arc.connection.get_default()
 
-for service_name in (arc.defaults.AFE_SERVICE_NAME,
-                     arc.defaults.TKO_SERVICE_NAME):
+for service_name in arc.shared.rpc.PATHS:
     version = c.service_interface_versions.get(service_name, None)
     if version is not None:
         print("%s interface version: %s" % (service_name, version))
