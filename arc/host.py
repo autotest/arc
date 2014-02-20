b0b10ce6ee45746f6e383b7af9d8ac5781c245f5
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-02-20 01:02:30 -0300

    arc.host: Fix oddly indented docstring
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- arc/host.py
+++ arc/host.py
@@ -95,7 +95,7 @@ def add(connection, name, status=None, locked=None, protection=None):
 
 def modify(connection, identification, **data):
     """
-   Modify a host entry
+    Modify a host entry
 
     :param connection:
     :param identification:
